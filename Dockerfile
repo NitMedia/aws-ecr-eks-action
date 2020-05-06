bb832d23326260e476fb62cb1fff1229b20c87d3
Nithin Meppurathu <nithin@nitmedia.com>
2020-05-06 13:05:37 -0400

    wip, lets try it out

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM ubuntu:latest
+
+LABEL "com.github.actions.name"="aws-ecr-eks-action"
+LABEL "com.github.actions.description"="logs into aws, build and pushes to ecr, allows to run kubectl"
+LABEL "com.github.actions.repository"="https://github.com/NitMedia/aws-ecr-eks-action"
+LABEL "com.github.actions.maintainer"="Nithin Meppurathu <nithin@nitmedia.com>"
+LABEL "com.github.actions.icon"="cloud"
+LABEL "com.github.actions.color"="red"
+
+ADD entrypoint.sh /entrypoint.sh
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -e
+
+export AWS_HOME=${{ runner.temp }}/.aws
+export AWS_CONFIG_FILE=${{ runner.temp }}/.aws/config
+export AWS_SHARED_CREDENTIALS_FILE=${{ runner.temp }}/.aws/credentials
+export AWS_REGION=${{ secrets.AWS_REGION }}
+
+# Set the PATH to include our binaries
+mkdir -p "${HOME}/.local/bin"
+export PATH="${HOME}/.local/bin:${PATH}"
+echo "::set-env name=PATH,::${PATH}"
+# Configure AWS
+mkdir -p "${AWS_HOME}"
+echo "::set-env name=AWS_CONFIG_FILE,::${AWS_CONFIG_FILE}"
+echo "::set-env name=AWS_SHARED_CREDENTIALS_FILE,::${AWS_SHARED_CREDENTIALS_FILE}"
+aws configure set default.region $AWS_REGION
+aws configure set default.output json
+aws configure set aws_access_key_id ${{ secrets.AWS_ACCESS_KEY_ID }}
+aws configure set aws_secret_access_key ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+# Validate AWS credentials
+aws sts get-caller-identity
+#Build, tag, and push image to Amazon ECR
+# Login to AWS ECR
+$( aws ecr get-login --region $AWS_REGION --no-include-email )
+docker build -t $ECR_REGISTRY/$ECR_REPOSITORY:$IMAGE_TAG .
+docker push $ECR_REGISTRY/$ECR_REPOSITORY:$IMAGE_TAG
+#Setup Kube Context
+export KUBECONFIG=${{ runner.temp }}/.kube/config
+# Setup AWS IAM Authenticator for Kubernetes
+cd $( mktemp -d )
+curl -o aws-iam-authenticator --location https://amazon-eks.s3-us-west-2.amazonaws.com/1.13.7/2019-06-11/bin/linux/amd64/aws-iam-authenticator
+chmod +x ./aws-iam-authenticator
+mv ./aws-iam-authenticator "${HOME}/.local/bin"
+aws-iam-authenticator help
+# Setup kustomize
+cd $( mktemp -d )
+curl -o kustomize --location https://github.com/kubernetes-sigs/kustomize/releases/download/v3.1.0/kustomize_3.1.0_linux_amd64
+chmod u+x ./kustomize
+mv ./kustomize "${HOME}/.local/bin"
+# Setup Kube Config
+mkdir -p "${RUNNER_TEMP}/.kube"
+echo "::set-env name=KUBECONFIG,::${KUBECONFIG}"
+echo "${{ secrets.KUBE_CONFIG_DATA }}" | base64 --decode > "${KUBECONFIG}"
