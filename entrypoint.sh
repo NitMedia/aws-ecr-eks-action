c0bfda6ac3cae46138719349790be884f0f2230d
Nithin Meppurathu <nithin@nitmedia.com>
2020-05-06 14:08:57 -0400

    pass as args?

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,19 @@ set -e
 export AWS_HOME=$RUNNER_TEMP/.aws
 export AWS_CONFIG_FILE=$RUNNER_TEMP/.aws/config
 export AWS_SHARED_CREDENTIALS_FILE=$RUNNER_TEMP/.aws/credentials
+export AWS_REGION=$1
+export AWS_ACCESS_KEY_ID=$2
+export AWS_SECRET_ACCESS_KEY=$3
+export ECR_REGISTRY=$4
+export ECR_REPOSITORY=$5
+export IMAGE_TAG=$6
+export KUBE_CONFIG=$7
 
 # Set the PATH to include our binaries
 mkdir -p "${HOME}/.local/bin"
 export PATH="${HOME}/.local/bin:${PATH}"
 echo "::set-env name=PATH,::${PATH}"
+
 # Configure AWS
 mkdir -p "${AWS_HOME}"
 echo "::set-env name=AWS_CONFIG_FILE,::${AWS_CONFIG_FILE}"
@@ -17,6 +25,7 @@ aws configure set default.region $AWS_REGION
 aws configure set default.output json
 aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
 aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+
 # Validate AWS credentials
 aws sts get-caller-identity
 #Build, tag, and push image to Amazon ECR
@@ -25,7 +34,7 @@ $( aws ecr get-login --region $AWS_REGION --no-include-email )
 docker build -t $ECR_REGISTRY/$ECR_REPOSITORY:$IMAGE_TAG .
 docker push $ECR_REGISTRY/$ECR_REPOSITORY:$IMAGE_TAG
 #Setup Kube Context
-export KUBECONFIG=${{ runner.temp }}/.kube/config
+export KUBECONFIG=$RUNNER_TEMP/.kube/config
 # Setup AWS IAM Authenticator for Kubernetes
 cd $( mktemp -d )
 curl -o aws-iam-authenticator --location https://amazon-eks.s3-us-west-2.amazonaws.com/1.13.7/2019-06-11/bin/linux/amd64/aws-iam-authenticator
